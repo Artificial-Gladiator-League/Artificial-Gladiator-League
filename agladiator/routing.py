12b1e61bcaaa24113122c55e2940e5413e5754d0
Chaim Duchovny <chaim.duchovny@gmail.com>
2026-05-12 12:53:58 +0300

    Fix Base.html

--- agladiator/routing.py
+++ agladiator/routing.py
@@ -1,23 +1,29 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agladiator.settings')
+
+# CRITICAL: Load Django BEFORE model imports
+import django
+django.setup()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
+# NOW safe - apps loaded
 from apps.core.routing import websocket_urlpatterns as core_ws
 from apps.games.routing import websocket_urlpatterns as games_ws
 from apps.tournaments.routing import websocket_urlpatterns as tournaments_ws
 
 django_asgi_app = get_asgi_application()
 
-application = ProtocolTypeRouter(
-    {
-        "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(
-                URLRouter(
-                    games_ws + tournaments_ws + core_ws
-                )
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                games_ws + tournaments_ws + core_ws
             )
         ),
-    }
-)
+    ),
+})
